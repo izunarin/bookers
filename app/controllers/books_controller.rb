ae42b754dbafefae90f08b5a853f3053c9e3b0e6
izunarin <112565053+izunarin@users.noreply.github.com>
2022-10-16 10:51:30 +0000

    [add]indexまで終わり

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,12 @@ class BooksController < ApplicationController
   def new
     
   end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to 'books'
+  end
 
   def index
     @book = Book.new
@@ -15,14 +21,22 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book .update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
   
   def book_params
-    params.require(:book).permit(:title, :bodt)
+    params.require(:book).permit(:title, :body)
   end
 end
