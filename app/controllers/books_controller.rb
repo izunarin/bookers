de12d05815e774d102d41c6fbacf1289a3c69d74
izunarin <112565053+izunarin@users.noreply.github.com>
2022-10-18 13:09:30 +0000

    [Finish ]3-12終わり

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,18 +6,24 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to 'books'
+    redirect_to '/books'
   end
 
   def index
     @book = Book.new
     @books = Book.all
+    
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Book was successfully creatad."
+    redirect_to book_path(@book.id)
+    else
+      @books= Book.all
+    render :index
+    end
   end
 
   def show
@@ -29,9 +35,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book .update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:success] ="Book was successfully updated."
+    redirect_to book_path(@book.id)
+    else
+       render :edit
+    end
   end
   
   private
